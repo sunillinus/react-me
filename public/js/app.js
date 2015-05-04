3947925a40ae84e4a94951fdd5abbb07782d71e1
sunillinus <sunil.linus@gmail.com>
2015-05-04 15:36:58 -0700

    React playlist/song components

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,43 @@
 (function() {
   'use strict';
 
-  var App = React.createClass({
+  var playlist = {
+    title: 'Dancelist',
+    songs: [
+      {title: 'Billie Jean', artist: 'Michael Jackson'},
+      {title: 'Love Shack', artist: 'The B-52s'},
+      {title: 'Give Me Everything', artist: 'Pitbull'}
+    ]
+  };
+
+  var Playlist = React.createClass({
     render: function() {
-      return <div>Hello</div>;
+      return (
+              <div>
+                <h3>{this.props.data.title}</h3>
+                <Song data={this.props.data.songs[0]} />
+                <Song data={this.props.data.songs[1]} />
+                <Song data={this.props.data.songs[2]} />
+              </div>
+              );
     }
   })
 
-  React.render(<App />, document.getElementById('app'));
+  var Song = React.createClass({
+    render: function() {
+      return (
+              <p>{this.props.data.title} - {this.props.data.artist}</p>
+              );
+    }
+  })
 
+  var App = React.createClass({
+    render: function() {
+      return (
+                <Playlist data={this.props.playlist} />
+              );
+    }
+  })
+
+  React.render(<App playlist={playlist} />, document.getElementById('app'));
 })();
