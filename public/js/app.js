84acf164d1d2ff5627b3cfa1359f3653cc76ff19
sunillinus <sunil.linus@gmail.com>
2015-05-05 00:49:57 -0700

    Moar react..

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,8 @@
     songs: [
       {title: 'Billie Jean', artist: 'Michael Jackson'},
       {title: 'Love Shack', artist: 'The B-52s'},
-      {title: 'Give Me Everything', artist: 'Pitbull'}
+      {title: 'Give Me Everything', artist: 'Pitbull'},
+      {title: 'Style', artist: 'Taylor Swift'}
     ]
   };
 
@@ -16,13 +17,13 @@
       return (
               <div>
                 <h3>{this.props.data.title}</h3>
-                <Song data={this.props.data.songs[0]} />
-                <Song data={this.props.data.songs[1]} />
-                <Song data={this.props.data.songs[2]} />
+                {this.props.data.songs.map(function(song, index) {
+                  return <Song data={song} key={index} />;
+                })}
               </div>
               );
     }
-  })
+  });
 
   var Song = React.createClass({
     render: function() {
@@ -30,15 +31,40 @@
               <p>{this.props.data.title} - {this.props.data.artist}</p>
               );
     }
-  })
+  });
+
+  var Timer = React.createClass({
+    render: function() {
+      return <p>{this.props.time}</p>
+    }
+  });
 
   var App = React.createClass({
+    getInitialState: function() {
+      var state = {time: new Date().toString()};
+      setInterval(function() {
+        this.setState({time: new Date().toString()});
+      }.bind(this), 1000);
+      return state;
+    },
+
+    getDefaultProps: function() {
+      var styles = {
+        background: "url(http://stylishhdwallpapers.com/wp-content/uploads/2014/10/Bloom-water-drops-leaf-HD-Wallpaper.jpg)",
+        color: "#fff"
+      };
+      return {styles: styles};
+    },
+
     render: function() {
       return (
+              <div id="app" style={this.props.styles}>
+                <Timer time={this.state.time} />
                 <Playlist data={this.props.playlist} />
+              </div>
               );
     }
   })
 
-  React.render(<App playlist={playlist} />, document.getElementById('app'));
+  React.render(<App playlist={playlist} />, document.body);
 })();
